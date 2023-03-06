4ed8c74e498e72e22f54aced8feb922f70917c1e
chaewonkong <chaewonkong@nexon.co.kr>
2023-03-06 11:16:23 +0900

    feat: user domain

---
+++ domain/user.go
@@ -0,0 +1,7 @@
+package domain
+
+// User 사용자 entity
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
---
+++ domain/user_repository.go
@@ -0,0 +1,47 @@
+package domain
+
+import "errors"
+
+// ErrNotFound 데이터를 찾을 수 없는 경우 error
+var ErrNotFound = errors.New("Not Found")
+
+type (
+	// UserRepository 사용자 repository
+	UserRepository interface {
+		FindAll() ([]User, error)
+		FindOne(name string) (User, error)
+	}
+
+	userRepositry struct {
+		users []User
+	}
+)
+
+// NewUserRepository UserRepository 생성자
+func NewUserRepository() UserRepository {
+	users := []User{
+		{Name: "Leon", Age: 31},
+		{Name: "Jane", Age: 29},
+	}
+	return userRepositry{users}
+}
+
+// FindAll 모든 사용자를 조회하는 메서드
+func (u userRepositry) FindAll() (result []User, err error) {
+	result = u.users
+	return
+}
+
+// FindOne 사용자의 name으로 특정 사용자를 조회하는 메서드
+func (u userRepositry) FindOne(name string) (result User, err error) {
+	for _, user := range u.users {
+		if name == user.Name {
+			result = user
+			return
+		}
+	}
+
+	// user를 찾지 못한 경우
+	err = ErrNotFound
+	return
+}
